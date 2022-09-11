f5424d3c44fb651994f97821a6de08d8e43a86b8
Naithar01 <eeung025@gmail.com>
2022-09-12 03:27:11 +0900

    Get List, Create, Patch 기능 추가

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+}
+
 func main() {
-	fmt.Println("Hello World")
+	var todos = []Todo{}
 
 	app := fiber.New()
 
-	app.Get("/helloworld", func(c *fiber.Ctx) error {
-		return c.SendString("Ok")
+	app.Get("/api/todo", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(todos)
+	})
+
+	app.Post("/api/todo", func(c *fiber.Ctx) error {
+		var newTodo = &Todo{}
+
+		if err := c.BodyParser(newTodo); err != nil {
+			return err
+		}
+
+		newTodo.ID = len(todos) + 1
+
+		todos = append(todos, *newTodo)
+
+		return c.Status(201).JSON(todos)
+	})
+
+	app.Patch("/api/todo/:id", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("Invalid id")
+		}
+
+		for index, todo := range todos {
+			if todo.ID == id {
+				todos[index].Done = !todos[index].Done
+				break
+			}
+		}
+
+		return c.JSON(todos)
 	})
 
 	log.Fatal(app.Listen(":4000"))
