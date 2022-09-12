60523f486158f59746bc68067e84ec7db4a12079
Naithar01 <eeung025@gmail.com>
2022-09-13 00:08:46 +0900

    Category, Post Crud 50% 구현

--- main.go
+++ main.go
@@ -6,50 +6,73 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-type Todo struct {
-	ID    int    `json:"id"`
+type Category struct {
+	Id    int    `json:"id"`
 	Title string `json:"title"`
-	Done  bool   `json:"done"`
+	Posts []Post `json:"posts"`
+}
+
+type Post struct {
+	Id int `json:"id"`
 }
 
 func main() {
-	var todos = []Todo{}
+	var categorys = []Category{}
+	var posts = []Post{}
 
 	app := fiber.New()
 
-	app.Get("/api/todo", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(todos)
+	app.Get("/api/category", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(categorys)
 	})
 
-	app.Post("/api/todo", func(c *fiber.Ctx) error {
-		var newTodo = &Todo{}
+	app.Get("/api/post", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(posts)
+	})
 
-		if err := c.BodyParser(newTodo); err != nil {
+	app.Post("/api/category", func(c *fiber.Ctx) error {
+		var newCategory = &Category{}
+
+		if err := c.BodyParser(newCategory); err != nil {
 			return err
 		}
 
-		newTodo.ID = len(todos) + 1
+		newCategory.Id = len(categorys) + 1
 
-		todos = append(todos, *newTodo)
+		categorys = append(categorys, *newCategory)
 
-		return c.Status(201).JSON(todos)
+		return c.Status(201).JSON(newCategory)
 	})
 
-	app.Patch("/api/todo/:id", func(c *fiber.Ctx) error {
+	app.Post("/api/post", func(c *fiber.Ctx) error {
+		var newPost = &Post{}
+
+		if err := c.BodyParser(newPost); err != nil {
+			return err
+		}
+
+		newPost.Id = len(posts) + 1
+
+		posts = append(posts, *newPost)
+
+		return c.Status(201).JSON(newPost)
+	})
+
+	app.Get("/api/post/:id", func(c *fiber.Ctx) error {
 		id, err := c.ParamsInt("id")
 
 		if err != nil {
 			return c.Status(401).SendString("Invalid id")
 		}
 
-		for index, todo := range todos {
-			if todo.ID == id {
-				todos[index].Done = !todos[index].Done
-				break
+		for index, post := range posts {
+			if post.Id == id {
+				return c.JSON(posts[index])
 			}
 		}
 
-		return c.JSON(todos)
+		return c.Status(401).SendString("Cant find Post")
+
 	})
 
 	log.Fatal(app.Listen(":4000"))
